0b0018185c2ff879a38d4dfe63a44c087c3263a0
Cristopher Justyn Tigua Vivero <86115235+CrisJ117@users.noreply.github.com>
2021-07-12 14:09:42 -0500

    Update colaborador5.h
    Se calculo la media y la varianza de la matriz con la finalidad de visualizar la cantidad mayor de ambas.

--- colaborador5.h
+++ colaborador5.h
@@ -1,7 +1,41 @@
 void mediayvarianza(float (*m1)[5], float (*m2)[])
 {
 
-cout<<"Determinar  cual de las 2 matrices tiene la mayor media y varianza";
+cout<<"\=============CALCULAR LA MEDIA Y VARIANZA=============\n";
+     cout<<"\nCuantos numeros son? ";
+     cin>>cantidad;
+     
+     sumatoria = new int[cantidad];
+     
+       for(int i= 0;i < cantidad;i++)
+    {
+     cout<<"Escribe el valor "<<i+1<<":";
+     cin>>sumatoria[i];
+    }
+    
+       for(int e = 0;e < cantidad;e++)
+    {
+       media = media + sumatoria[e];
+    }
+    
+       media = media / cantidad;
+    
+    system("cls");
+    
+     cout<<"La Media es: ";
+     cout<<media<<endl;
+    
+       for(int k = 0;k < cantidad;k++)
+    {
+     varianza = varianza + (((sumatoria[k])-(media))*((sumatoria[k])-(media)));
+         
+    }
+    }
+     varianza = varianza / cantidad;d
+     cout<<"la varianza es: ";
+     cout<<varianza<<endl;
+     cout<<"La desviacion estandar es +/-: ";
+     cout<<sqrt(varianza);
 
 
 
