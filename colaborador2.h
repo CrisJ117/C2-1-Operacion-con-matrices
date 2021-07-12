c7d9582d384943c3a396ef8c930c9d3e9d060828
Kevin Antonio Tapuy Cañola <86172650+ke212002@users.noreply.github.com>
2021-07-12 16:45:35 -0500

    Create colaborador2.h
    creación de codificación del  producto de dos matrices

--- colaborador2.h
+++ colaborador2.h
@@ -1,9 +1,28 @@
 void producto(float (*m1)[5], float (*m2)[5], float (*r)[5])
 {
 
-cout<<"Obetener el producto de las dos matrices ";
+  cout<<"Obetener el producto de las dos matrices ";
+  for(int i=0; i<5; ++i){
+
+      for(int j=0; j<5; ++j){
+        r[i] [j] = 0;
+    }
+    }
+      for(int i=0; i<5; ++ i){
+        for(int j=0; j<=5; ++j){
+         for(int z=0; z<5; ++z)
+          r[i][j] += m1[i][z] * m2[z][j];
+    }
+	
+    }	
+	producto(matriz1,matriz2,resultado);
+	  mostrar(resultado);
+		 break;
+	producto(matriz1,matriz2,resultado);
+	  mostrar(resultado);
+		 break;
+  }
 
 
 
 
-}
