731f7c35011629724bae7c5b32766e1c53940cbe
genesi Simisterra <86116060+SAMY69xx@users.noreply.github.com>
2021-07-13 16:35:19 -0500

    Update colaborador1.h
    yo Visualice los valores de la matriz 1 y la matriz dos, utilice el for para indicar la posición de la matriz y así poder cumplir con el enunciado

--- colaborador1.h
+++ colaborador1.h
@@ -1,9 +1,17 @@
 void ingresar(float (*m1)[5], float (*m2)[5])
 {
 
-cout<<"Ingresar y mostrar los valores de los arreglos o matrices";
-
-
-
-
-}
+ 
+	cout<<"\=======INGRESAR LOS VALORES DE LAS MATRICES=======\n";
+	 float a, ingresar;
+      for(int i = 0; i < 5; i++){
+    cout << "\n Ingresar valor " << i + 1 << "****:\n";
+      for(int j = 0; j < 5; j++){
+	cout << "\n posicion " << i + 1 << "****:\n";
+	cin >> a;
+    matriz[i][j] = a;
+	}
+	ingresar(matriz1,matriz2);
+	mostrar(matriz1);
+	mostrar(matriz2);
+		break;
